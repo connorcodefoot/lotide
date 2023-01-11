8e63eb88e70ce0f26f4307f306a38a00758f609f
Connor <cgbroadfoot@gmail.com>
2023-01-11 17:23:47 +0000

    eslint checked file

--- middle.js
+++ middle.js
@@ -1,15 +1,15 @@
 const eqArrays = function(arr1, arr2) {
 
   if (arr1.length !== arr2.length) {
-    return false
-  } 
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const assertArraysEqual = function(actual, expected) {
 
@@ -20,45 +20,36 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
+const middle = function(arr) {
 
-// Function that finds the middle of an array
-// If function has less than 2 values, return an empty array
-// For arrays with odd numbers, an array with a single element should be returned
-// For arrays with even numbers, an array with two elements should be returned
+  let outputArr = [];
 
-// Test the middle function
+  if (arr.length > 2) {
 
-const middle = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
 
-  let outputArr = []
-
-  if(arr.length > 2) {
-
-    for(let i = 0; i < arr.length; i++){
-
-      if(arr.length % 2 !== 0) {
-        let mid = arr.length / 2
-        console.log(mid)
-        outputArr.push(arr[Math.floor(mid)])
-        break
+      if (arr.length % 2 !== 0) {
+        let mid = arr.length / 2;
+        outputArr.push(arr[Math.floor(mid)]);
+        break;
       }
 
-      let mid1 = arr.length / 2
-      let mid2 = (arr.length / 2) - 1
-      outputArr.push(arr[Math.floor(mid1)], arr[Math.floor(mid2)])
-      break
+      let mid1 = arr.length / 2;
+      let mid2 = (arr.length / 2) - 1;
+      outputArr.push(arr[Math.floor(mid1)], arr[Math.floor(mid2)]);
+      break;
     }
   }
-  return outputArr
+  return outputArr;
 };
 
-console.log('TEST CASES')
+console.log('TEST CASES');
 
 // AssertArrayEquals Test
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 4]), false)
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
 
 // Middle Test Odd
-console.log('Middle Test Odd:', middle([2, 5, 25, 4,9]))
+console.log('Middle Test Odd:', middle([2, 5, 25, 4,9]));
 
 // Middle Test Even
-console.log('Middle Test Even:', middle([2, 5, 25, 13, 4, 9]))
+console.log('Middle Test Even:', middle([2, 5, 25, 13, 4, 9]));
