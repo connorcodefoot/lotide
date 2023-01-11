e5b83422c0dfb29b13981f33e20d72b682700322
Connor <cgbroadfoot@gmail.com>
2023-01-11 22:43:28 +0000

    Added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,29 @@
+const countLetters = function(str) {
+
+  let letterIndexObj = {};
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] !== ' ') {
+  
+      if (!(letterIndexObj[str[i]])) {
+        letterIndexObj[str[i]] = []
+      }
+
+      letterIndexObj[str[i]].push(i)
+
+    }
+  }
+  return letterIndexObj;
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♂️🙅‍♂️🙅‍♂️🙅‍♂️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(countLetters('this friggen string'));
