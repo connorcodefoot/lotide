4251ecf4d9668b6a1d7c849d2589cfd41e7e2592
Connor <cgbroadfoot@gmail.com>
2023-01-12 04:15:15 +0000

    added assertObjectsEqual.js to repo

---
+++ assertObjectsEqual.js
@@ -0,0 +1,61 @@
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♂️🙅‍♂️🙅‍♂️🙅‍♂️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false
+  } 
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true
+}
+
+const assertObjectsEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅‍♂️🙅‍♂️🙅‍♂️🙅‍♂️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key])
+    }
+
+    if (object1[key] !== object2[key]) {
+      return false
+    }
+  }
+  return true
+};
+
+//Test Cases
+console.log('TEST CASES')
+const multiColorShirtObject = { colors: ["red", "green"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "green"] };
+assertObjectsEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true) // => true
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertObjectsEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
